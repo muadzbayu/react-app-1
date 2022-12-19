aebf8e19d50384361a9138810bc44c5307cc2112
muadzbayu <muadz.bayual@gmail.com>
2022-12-19 15:00:34 +0700

    added contact and about menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import Header from "./components/Header"
+import ProductList from "./components/ProductList";
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([
+    { id: 1, title: 'Baju', price: 500000 },
+    { id: 2, title: 'Celana', price: 400000 },
+    { id: 3, title: 'Jacket', price: 1000000 }
+  ])
+
+  const [name, setName] = useState('Fikri')
+
+  const deleteProduct = (productId) => {
+    const newProducts = products.filter(product => product.id !== productId)
+    setProducts(newProducts)
+  }
+
+  useEffect(() => {
+    console.log('Use Effect Running')
+  }, [name])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ProductList products={products} deleteProduct={deleteProduct} />
+      <button onClick={() => setName('John')}>Change Name</button>
+      <p>Name: {name}</p>
     </div>
   );
 }
